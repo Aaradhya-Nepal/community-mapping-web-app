abb261d85c5732fb8a94a9c7df657449025bb4e2
aaradhya <aaradhyanepal1@gmail.com>
2024-10-06 02:27:23 +0545

    update weather api

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,7 @@ const config = {
     GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY as string,
     OPEN_WEATHER_API_URL: process.env.NEXT_PUBLIC_OPEN_WEATHER_API_URL as string,
     OPEN_WEATHER_API_KEY: process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY as string,
+    OPEN_METEO_API_URL: process.env.NEXT_PUBLIC_OPEN_METEO_API_URL as string,
 }
 
 export default config;
--- src/queries/weather.ts
+++ src/queries/weather.ts
@@ -2,10 +2,10 @@ import {useQuery} from "react-query"
 import QUERY_KEYS from "@/constants/queryKeys"
 import {getAll} from "@/api/weather"
 
-export function useGetWeatherData({lat, lon, enabled = true}: any) {
+export function useGetWeatherData({latitude, longitude, enabled = true}: any) {
     return useQuery({
-        queryKey: [QUERY_KEYS.weathers, lat, lon],
-        queryFn: () => getAll({lat, lon}),
+        queryKey: [QUERY_KEYS.weathers, latitude, longitude],
+        queryFn: () => getAll({latitude, longitude}),
         retry: false,
         keepPreviousData: true,
         enabled: enabled,
