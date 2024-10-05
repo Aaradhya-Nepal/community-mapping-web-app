d3a68ba4e8bd50fc3c58f6a8c02cf691ac288903
aaradhya <aaradhyanepal1@gmail.com>
2024-10-05 21:50:06 +0545

    feature: add env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,8 @@
 const config = {
     GOOGLE_API_URL: process.env.NEXT_PUBLIC_GOOGLE_API_URL as string,
     GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY as string,
+    OPEN_WEATHER_API_URL: process.env.NEXT_PUBLIC_OPEN_WEATHER_API_URL as string,
+    OPEN_WEATHER_API_KEY: process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY as string,
 }
 
 export default config;
