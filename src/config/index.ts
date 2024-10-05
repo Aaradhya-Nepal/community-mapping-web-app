9d97b4441a11f7901ab40668901729ab33594783
aaradhya <aaradhyanepal1@gmail.com>
2024-10-05 17:40:36 +0545

    feature: create config api key

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const config = {
-    GOOGLE_API_URL: process.env.NEXT_PUBLIC_GOOGLE_API_URL,
-    GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+    GOOGLE_API_URL: process.env.NEXT_PUBLIC_GOOGLE_API_URL as string,
+    GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY as string,
 }
 
 export default config;
