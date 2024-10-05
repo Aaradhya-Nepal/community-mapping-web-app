b0e4dbb59f3f8254f809ba09f6f90c74bae56834
aaradhya <aaradhyanepal1@gmail.com>
2024-10-05 17:41:07 +0545

    fetch api cleanly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,15 @@
 "use client";
-
-import {useEffect} from 'react';
-import MapInterface2 from "@/components/maps/MapInterface2";
+import MapInterface from "@/components/maps/MapInterface";
+import {useGetGeocode} from "@/queries/geocode";
 
 export default function Home() {
-    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
     const address = '1600 Amphitheatre Parkway, Mountain View, CA';
-
-    const fetchGeocode = async () => {
-        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error('Error fetching geocode data:', error);
-        }
-    };
-
-    useEffect(() => {
-        fetchGeocode();
-    }, []);
+    const {data: geocodes} = useGetGeocode(address);
+    console.log(geocodes)
 
     return (
         <>
-            <MapInterface2/>
+            <MapInterface/>
         </>
     );
 }
